e972e704a34d33de4a0cc952bf32d87cec9ca29c
dhenwood <dhenwood@users.noreply.github.com>
2025-05-06 10:53:45 +1000

    Add files via upload

--- macro-Wallpaper.js
+++ macro-Wallpaper.js
@@ -2,60 +2,35 @@ import xapi from 'xapi';
 
 const username = "dynamicWallpaper" // local username account
 const password = "C!sco123" // local password account
-var ipAddress = "192.168.0.15"
+var ipAddress = "192.168.0.19" // update to reflect the video device IP address
+const mode = "obtp" // if OBTP is setup, set to 'obtp', otherwise set to 'time'
 
 
 xapi.event.on('UserInterface Extensions Widget Action', (event) => {
   if(event.WidgetId == 'staticImages' && event.Type == 'pressed'){
-    if (event.Value == '1'){
-      const url = "https://www.employees.org/~dhenwood/WbxWallpaper/wallpaperForestRiver.zip"
-      updateWallpaper(url)
-    }else if (event.Value == '2'){
-      const url = "https://www.employees.org/~dhenwood/WbxWallpaper/wallpaperBeachSand.zip"
-      updateWallpaper(url)
-    }else if (event.Value == '3'){
-      const url = "https://www.employees.org/~dhenwood/WbxWallpaper/wallpaperWaterfall.zip"
-      updateWallpaper(url)
-    }
+    const selectedButton = event.Value
+    const url = `https://www.employees.org/~dhenwood/WbxWallpaper/${selectedButton}.zip`
+    updateWallpaper(url)
     clearPanel();
-  }else if(event.WidgetId == 'liveVideoTime' && event.Type == 'pressed'){
-    if (event.Value == '1'){
-      var video = "Smoke360"
-      const url = `https://www.employees.org/~dhenwood/video/time.html?ipAddress=${ipAddress}&username=${username}&password=${password}&video=${video}`
-      console.log("url: " + url)
-      xapi.command("UserInterface WebView Display", {Url: url, Title: "Live Wallpaper"})
-    }else if (event.Value == '2'){
-      var video = "webexDesk"
-      const url = `https://www.employees.org/~dhenwood/video/time.html?ipAddress=${ipAddress}&username=${username}&password=${password}&video=${video}`
-      console.log("url: " + url)
-      xapi.command("UserInterface WebView Display", {Url: url, Title: "Live Wallpaper"})
-    }else if (event.Value == '3'){
-      var video = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny"
-      const url = `https://www.employees.org/~dhenwood/video/time.html?ipAddress=${ipAddress}&username=${username}&password=${password}&video=${video}`
-      console.log("url: " + url)
-      xapi.command("UserInterface WebView Display", {Url: url, Title: "Live Wallpaper"})
-    }
-    clearPanel();
-  }else if(event.WidgetId == 'liveVideoObtp' && event.Type == 'pressed'){
-    if (event.Value == '1'){
-      var video = "Oregon"
-      const url = `https://www.employees.org/~dhenwood/video/obtp.html?ipAddress=${ipAddress}&username=${username}&password=${password}&video=${video}`
-      clearPanel();
-      xapi.command("UserInterface WebView Display", {Url: url, Title: "Live Wallpaper"})
-      notifyUI("Updating Wallpaper", "This video is large, so it may take a while to load ⏱️", 8)
-    }else if (event.Value == '2'){
-      var video = "LakeFire"
-      const url = `https://www.employees.org/~dhenwood/video/obtp.html?ipAddress=${ipAddress}&username=${username}&password=${password}&video=${video}`
-      clearPanel();
-      xapi.command("UserInterface WebView Display", {Url: url, Title: "Live Wallpaper"})
-    }else if (event.Value == '3'){
-      var video = "Utah"
-      const url = `https://www.employees.org/~dhenwood/video/obtp.html?ipAddress=${ipAddress}&username=${username}&password=${password}&video=${video}`
-      clearPanel();
-      xapi.command("UserInterface WebView Display", {Url: url, Title: "Live Wallpaper"})
+
+  }else if(event.WidgetId == 'landscapeVideos' && event.Type == 'pressed'){
+    const video = event.Value
+    const url = `https://www.employees.org/~dhenwood/video/${mode}.html?ipAddress=${ipAddress}&username=${username}&password=${password}&video=${video}`
+    xapi.command("UserInterface WebView Display", {Url: url, Title: "Dynamic Wallpaper"})
+    if(video == "Oregon" || video == "Utah"){
       notifyUI("Updating Wallpaper", "This video is large, so it may take a while to load ⏱️", 8)
     }
     clearPanel();
+
+  }else if(event.WidgetId == 'otherVideos' && event.Type == 'pressed'){
+    var video = event.Value
+    if (video == "BigBuckBunny"){
+      video = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny"
+    }
+    const url = `https://www.employees.org/~dhenwood/video/${mode}.html?ipAddress=${ipAddress}&username=${username}&password=${password}&video=${video}`
+    xapi.command("UserInterface WebView Display", {Url: url, Title: "Dynamic Wallpaper"})
+    clearPanel();
+
   }else if(event.WidgetId == 'screensaverClose' && event.Type == 'pressed'){
       xapi.command("WebEngine DeleteStorage")
       clearPanel();
@@ -70,8 +45,8 @@ function updateWallpaper(url){
 
 function clearPanel(){
   xapi.command("UserInterface Extensions Widget UnsetValue", {WidgetId: "staticImages"})
-  xapi.command("UserInterface Extensions Widget UnsetValue", {WidgetId: "liveVideoTime"})
-  xapi.command("UserInterface Extensions Widget UnsetValue", {WidgetId: "liveVideoObtp"})
+  xapi.command("UserInterface Extensions Widget UnsetValue", {WidgetId: "landscapeVideos"})
+  xapi.command("UserInterface Extensions Widget UnsetValue", {WidgetId: "otherVideos"})
 }
 
 function notifyUI(title, message, duration){
@@ -83,6 +58,7 @@ function notifyUI(title, message, duration){
 }
 
 async function init(){
+  // Check if allowDeviceCertificateStatus is enabled
   const allowDeviceCertificateStatus = await xapi.Config.WebEngine.Features.AllowDeviceCertificate.get()
   if(allowDeviceCertificateStatus == "False"){
     xapi.Config.WebEngine.Features.AllowDeviceCertificate.set("True");
